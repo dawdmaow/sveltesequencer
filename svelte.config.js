c02a9d04b87b9eb7ca0141d3060491d0dc629b10
Dawid <dawidkotlin@gmail.com>
2026-02-07 20:11:30 +0100

    ...

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,22 @@
 import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = { kit: { adapter: adapter() } };
+const config = {
+	kit: {
+		// adapter: adapter(),
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		}),
+		paths: {
+			base: process.env.BASE_PATH || (process.env.NODE_ENV === 'production' ? '/sveltesequencer' : '')
+		}
+	}
+};
 
 export default config;
